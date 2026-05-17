5e20be4b6fcdd8ee1af81c44fb1a44a4b5f03336
ricardodsgoes-sudo <ricardo.ds.goes@gmail.com>
2026-05-16 21:49:41 -0300

    logo app

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+requestAnimationFrame(() => {
+  const el = document.getElementById('preloader')
+  if (el) {
+    el.style.opacity = '0'
+    setTimeout(() => el.remove(), 500)
+  }
+})
